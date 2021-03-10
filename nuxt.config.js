a33847080e2d12cf3ce7ad4c57b37289662a1380
drumath <2237k.drumath@gmail.com>
2021-03-10 16:33:25 +0900

    :sparkles: viteでserveを確認＋nuxt-ts→nuxtにした(nuxtでtsのサポートがあるため)

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    'nuxt-vite',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
